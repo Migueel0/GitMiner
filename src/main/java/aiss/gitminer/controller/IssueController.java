9dbd42550813d49f64be2a3b570bf183b6282d02
PAULA LUNA <paulunnav@alum.us.es>
2023-05-04 16:16:54 +0200

    project setup

---
+++ src/main/java/aiss/gitminer/controller/IssueController.java
@@ -0,0 +1,7 @@
+package aiss.gitminer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IssueController {
+}
---
+++ src/main/java/aiss/gitminer/controller/UserController.java
@@ -0,0 +1,11 @@
+package aiss.gitminer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/gitminer/users")
+public class UserController {
+
+
+}
